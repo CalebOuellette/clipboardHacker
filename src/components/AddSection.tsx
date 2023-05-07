fc80f24a7e80623ea102fc978e4bf2a041f93064
Caleb Ouellette <caleb.ouellette@gmail.com>
2023-05-07 13:41:50 -0700

    Dropdown build

--- src/components/AddSection.tsx
+++ src/components/AddSection.tsx
@@ -1,10 +1,30 @@
 import { CLIPBOARD_DATA_TYPES } from "../lib/ClipboardTypeUtils";
-import {Dropdown} from './Dropdown';
+import { Dropdown } from "./Dropdown";
 
 export type CopyPayload = {
   type: string;
   content: string;
 };
+
+const PlusIcon = (
+  <svg
+    width="8"
+    height="8"
+    viewBox="0 0 8 8"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M2.9887 8V0H5.0113V8H2.9887ZM0 5.0113V2.9887H8V5.0113H0Z"
+      fill="white"
+    />
+  </svg>
+);
+
+const SectionOptions = CLIPBOARD_DATA_TYPES.map((type) => ({
+  name: type,
+  value: type,
+}));
 
 export default function AddSection({
   copyPayload,
@@ -13,32 +33,23 @@ export default function AddSection({
   copyPayload: () => CopyPayload[];
   setCopyPayload: (newCopyPayload: CopyPayload[]) => void;
 }) {
+  const addSection = (option: { name: string; value: string }) => {
+    const oldPayload = copyPayload();
+    const i = oldPayload.findIndex((entry) => entry.type === option.value);
+    if (i === -1) {
+      setCopyPayload([...oldPayload, { type: option.value, content: "" }]);
+    }
+  };
+
   return (
-    <div>
-      <Dropdown />
-      <div class="gap-3 flex flex-wrap justify-center">
-        {CLIPBOARD_DATA_TYPES.filter(
-          (type) =>
-            !copyPayload()
-              .map((p) => p.type)
-              .includes(type)
-        ).map((type) => {
-          return (
-            <button
-              class="bg-blue-500  hover:bg-blue-400 text-slate-50 font-bold py-2 px-4 rounded"
-              onClick={() => {
-                const oldPayload = copyPayload();
-                const i = oldPayload.findIndex((entry) => entry.type === type);
-                if (i === -1) {
-                  setCopyPayload([...oldPayload, { type, content: "" }]);
-                }
-              }}
-            >
-              {type}
-            </button>
-          );
-        })}
-      </div>
+    <div class="absolute">
+      <Dropdown
+        openUp
+        icon={PlusIcon}
+        options={SectionOptions}
+        onOptionSelect={addSection}
+        value={{ name: "Add Section", value: "" }}
+      />
     </div>
   );
 }
--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,13 +1,103 @@
-export const Dropdown = () => {
-  return (
-    <div class="bg-neutral-800 box-border flex flex-row justify-center relative h-8 items-center px-3 py-px rounded-lg">
-      <div class="text-sm font-['Inter'] font-bold text-white relative my-2">
-        Application/json
-      </div>
-      <img
-        src="https://file.rendit.io/n/cymuR6zkFV47K7jPEiTM.svg"
-        class="box-border relative w-2 min-w"
-      />
-    </div>
-  );
+import {
+  HeadlessDisclosureChild,
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "solid-headless";
+import { Accessor, Component, For, JSX } from "solid-js";
+import classNames from "classNames";
+
+const dropDownIcon = (
+  <svg
+    width="12"
+    height="9"
+    viewBox="0 0 12 9"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path d="M6 9L11.1962 0H0.803848L6 9Z" fill="white" />
+  </svg>
+);
+
+type DropDownOption = {
+  name: string;
+  value: string;
 };
+
+export const Dropdown: Component<{
+  onOptionSelect: (value: DropDownOption) => void;
+  icon?: JSX.Element;
+  value: DropDownOption;
+  options: DropDownOption[];
+  openUp?: boolean; // TODO toggle bottom-9 on listboxoptions
+}> = (
+  { onOptionSelect, openUp = false, icon = dropDownIcon, options, value },
+) => {
+    return (
+      <Listbox
+        defaultOpen={false}
+        value={value}
+        onSelectChange={(option: any) => onOptionSelect(option as DropDownOption)}
+      >
+        <ListboxButton class="gap-5 bg-neutral-800 hover:bg-neutral-600 hover:pointer box-border flex flex-row justify-center relative h-8 items-center px-3 py-px rounded-lg">
+          <div class="text-sm font-['Inter'] font-bold text-white relative my-2">
+            {value.name}
+          </div>
+          {icon}
+        </ListboxButton>
+        <HeadlessDisclosureChild>
+          {({ isOpen }) => (
+            isOpen() && (
+              <ListboxOptions class="absolute bottom-9 w-[300px] py-1 mt-1 overflow-auto text-base bg-white rounded-md shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                <For each={options}>
+                  {(person) => (
+                    <ListboxOption
+                      class="focus:outline-none group"
+                      value={person}
+                    >
+                      {({ isActive, isSelected }) => (
+                        <div
+                          class={classNames(
+                            isActive()
+                              ? "text-amber-900 bg-amber-100"
+                              : "text-gray-900",
+                            "group-hover:text-amber-900 group-hover:bg-amber-100",
+                            "cursor-default select-none relative py-2 pl-10 pr-4",
+                          )}
+                        >
+                          <span
+                            class={classNames(
+                              isSelected() ? "font-medium" : "font-normal",
+                              "block truncate",
+                            )}
+                          >
+                            {person.name}
+                          </span>
+                          {isSelected()
+                            ? (
+                              <span
+                                class={classNames(
+                                  isActive()
+                                    ? "text-amber-600"
+                                    : "text-amber-600",
+                                  "group-hover:text-amber-600",
+                                  "absolute inset-y-0 left-0 flex items-center pl-3",
+                                )}
+                              >
+                                check
+                              </span>
+                            )
+                            : null}
+                        </div>
+                      )}
+                    </ListboxOption>
+                  )}
+                </For>
+              </ListboxOptions>
+            )
+          )}
+        </HeadlessDisclosureChild>
+      </Listbox>
+    );
+  };
